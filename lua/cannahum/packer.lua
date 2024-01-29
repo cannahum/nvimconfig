1c9062e3d39653253f1ef76bf9ec90bdebe8337f
Jon Nahum <cannahum@outlook.com>
2024-01-29 14:01:37 -0500

    add toggleterm

--- lua/cannahum/packer.lua
+++ lua/cannahum/packer.lua
@@ -49,4 +49,7 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},
         }
     }
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end}
 end)
